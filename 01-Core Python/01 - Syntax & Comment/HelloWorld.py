05c1af8de0f806b06ebfaaab679c8cf7287f6763
mdmahfuz307 <njnirob2019@gmail.com>
2024-10-16 12:07:39 +0600

    Syntax & Comment

---
+++ 01-Core Python/01 - Syntax & Comment/HelloWorld.py
@@ -0,0 +1,14 @@
+print("Hello World")
+print('Hello Nirob')
+
+# (#) --> Comments starts with (#), and Python will ignore them.
+
+
+"""
+Since Python will ignore string literals that are not assigned to a variable, 
+you can add a multiline string (triple quotes) in your code, 
+and place your comment inside it
+"""
+
+print(420)
+print(3.1416)
